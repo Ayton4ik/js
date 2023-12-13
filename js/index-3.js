f12d7b6598cdaa74b30813e4a26813c51130c2a9
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2023-12-13 20:32:43 +0200

    ,

---
+++ js/index-3.js
@@ -0,0 +1,32 @@
+// const message = 'Привіт я арсеній';
+// console.log(message.length);
+// console.log(message.indexOf('я'));
+
+// конкатинація рядків
+
+// const firstName = 'Arseniy';
+// const lastName = 'Mykhalchuk';
+// const fullname = `${firstName} ${lastName}`;
+// const hello = `Hello, ${fullname}`;
+// console.log(hello);
+
+// Завдання 1
+
+/*
+ * Напиши скрипт який виведе рядок в форматі:
+ * «Гість x y поселяється в g номер q»,
+ * підставив замість x y g q значення змінних
+ */
+// const firstName = 'Mango';
+// const lastName = 'Lee';
+// const room = 716;
+// const type = 'VIP';
+// const customersMsg = `Гість ${firstName} ${lastName} заселяється в ${room} номер ${type}`;
+// console.log(customersMsg)
+
+// Завдання 2
+
+// const firstName = prompt("Введіть своє ім'я");
+// const lastName = prompt("Введіть ваше прізвище");
+// const age = prompt("Введіть ваш вік");
+// alert(`Користувач ${firstName} ${lastName} віком ${age} має доступ до аккаутну!`);
