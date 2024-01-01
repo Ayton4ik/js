5ff29c69577ffc9878123f81bd96f77c7f9f883e
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2024-01-01 20:25:35 +0200

    .

---
+++ js/index-8.js
@@ -0,0 +1,41 @@
+// for (let i = 0; i < 5; i += 1) {
+//     console.log(i);
+// }
+/*
+ * Напиши скрипт який підраховує загальну суму зп працівників.
+ * Кількість робітників зберігається в змінній employees.
+ * ЗП кожного працівника це випадкове число від 500 до 5000
+ * Записати суму в змінну totalSalary і вивести в консоль
+ */
+// 1. Створити змінні employees, totalSalary
+// 2. пройтися по зп працівників n кількість разів
+// 3. згенерувати для кожного працівника зп
+// 4. додати зп кожного працівника
+// 5. вивести totalSalary в консоль
+
+// const employees = 4;
+// let totalSalary = 0;
+// const minSalary = 500;
+// const maxSalary = 5000;
+
+// for (let i = 1; i <= employees; i += 1) {
+//     const salary = Math.round(Math.random() * (maxSalary - minSalary) + minSalary);
+//     console.log(`Зп працівника ${i}:`, salary);
+
+//     totalSalary += salary;
+// }
+
+// console.log(totalSalary);
+
+
+// const min = 10;
+// const max = 15;
+// let total = 0;
+
+// for (let i = min; i <= max; i +=1){
+//     if (i % 2 === 0){
+//         total += i;
+//     }
+// }
+// console.log(total);
+
