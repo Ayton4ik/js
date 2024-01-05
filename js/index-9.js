804802517247a2dc9e77e57ee96595e1abb9c523
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2024-01-05 20:35:07 +0200

    .

---
+++ js/index-9.js
@@ -0,0 +1,28 @@
+// for (let i = 1; i <= 5; i += 1){
+//     console.log(i);
+// }
+
+// const min = 1;
+// const max = 10;
+// for (let i = max; i >= min; i-=1){
+//     if(i === 6){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+// let min = 1;
+// let max = 20;
+// for (let i = min; i < max; i+=1){
+//     if(i % 2 !== 0){
+//         console.log(i)
+//     }
+// }
+
+
+// if (number > 100){
+//     alert(`Ваше число: ${number}`);
+// } else {
+//     alert(`Число ${number} менше ста((😥. Спробуйте знову`);
+// }
