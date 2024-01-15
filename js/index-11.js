9489fdb0e009d8bda70a3c328e42e3d9fbcc4158
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2024-01-15 20:30:17 +0200

    .

---
+++ js/index-11.js
@@ -0,0 +1,26 @@
+// Методи split() і join()
+
+// const str = 'Hello!';
+// const arrStr = str.split('');
+// console.log(arrStr);
+
+// const reversArr = arrStr.join('');
+// console.log(reversArr);
+
+// Методи: 1)push(Додає у кінець масиву);
+//         2)pop(Видаляє з кінця масиву);
+//         3)shift(Видаляє з початоку масиву);
+//         4)unshift(Додає на початок масиву);
+
+// Метод slice()
+// const names = ['Ola', 'Petra', 'Nastia', 'Mary'];
+// const sliceNames = names.slice(2, 3);
+// console.log(sliceNames);
+
+// Метод concat()
+
+// const arr1 = [5, 2, 6];
+// const names = ['Ola', 'Petra', 'Nastia', 'Mary'];
+// const concatetArrs = arr1.concat(names);
+// console.log(concatetArrs);
+
