8665baf7acf53b9cfc8b2134c2ec4f6bff33a661
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2024-01-22 20:31:09 +0200

    .

---
+++ js/index-12.js
@@ -0,0 +1,18 @@
+// const fnA = function(){
+//     console.log('Hello');
+// }
+
+// fnA();
+
+
+// Напиши функцию slugify(string) яка отримує рядок і повертає URL-slug
+//  Заголовок статті складається тільки з букв та пропусків
+
+
+// const slugify = function (string) {
+//     const slugTitle = title.toLocaleLowerCase().split(' ').join('-');
+// }
+
+// console.log(slugify('Top 10 benefits of React framework'));
+// console.log(slugify('Azure Static Web Apps are Awesome'));
+// console.log(slugify('Technical writing tips for non-native English speakers'));
