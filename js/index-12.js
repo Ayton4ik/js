2e7f29ca25ecbff692c47742efbadd89887e464d
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2024-01-26 19:11:25 +0200

    Update index-12.js

--- js/index-12.js
+++ js/index-12.js
@@ -9,10 +9,11 @@
 //  Заголовок статті складається тільки з букв та пропусків
 
 
-// const slugify = function (string) {
-//     const slugTitle = title.toLocaleLowerCase().split(' ').join('-');
-// }
+const slugify = function (string) {
+    const slugTitle = string.toLocaleLowerCase().split(' ').join('-');
+    return slugTitle
+}
 
-// console.log(slugify('Top 10 benefits of React framework'));
-// console.log(slugify('Azure Static Web Apps are Awesome'));
-// console.log(slugify('Technical writing tips for non-native English speakers'));
+console.log(slugify('Top 10 benefits of React framework'));
+console.log(slugify('Azure Static Web Apps are Awesome'));
+console.log(slugify('Technical writing tips for non-native English speakers'));
