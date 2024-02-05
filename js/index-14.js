6bbe719200e7b3b7cc004d86401d1fb2a55be7b7
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2024-02-05 20:27:33 +0200

    .

---
+++ js/index-14.js
@@ -0,0 +1,25 @@
+// const playList = {
+//     name: 'myPlayList',
+//     raiting: 10,
+//     tracks: ['track-1', 'track-2', 'track-3'],
+// }
+
+// console.log(playList);
+
+// console.log(playList.name);
+// console.log(playList['name']);
+
+// Задача 1
+
+// Створіть об'єкт "person" з властивостями "name", "age", "gender".
+
+// const person = {
+//     name: 'Арсеній',
+//     age: 14,
+//     gender: 'male,'
+// }
+
+// person.age += 1;
+// person.university = 'Verhovina';
+// console.log(person);
+
