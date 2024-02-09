b2dd9397e43a5b90ed1262980ce2f1c7aab17946
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2024-02-09 20:30:50 +0200

    Update index-14.js

--- js/index-14.js
+++ js/index-14.js
@@ -16,10 +16,54 @@
 // const person = {
 //     name: 'Арсеній',
 //     age: 14,
-//     gender: 'male,'
+//     gender: 'male'
 // }
 
 // person.age += 1;
 // person.university = 'Verhovina';
 // console.log(person);
 
+// const playlist = {
+//     name: 'Плейлист',
+//     rating: 5,
+//     tracks: ['трек-1', 'трек-2', 'трек-3'],
+//     changeName(newName){
+//         this.name = newName;
+//     },
+//     addTrack(track){
+//         this.tracks.push(track);
+//     },
+//     updateRating(newRating){
+//         this.rating = newRating;
+//     },
+//     getTrackCount(){
+//         return this.tracks.length;
+//     }
+// }
+
+// console.log(playlist);
+// playlist.changeName('Супер плейлист');
+
+// playlist.addTrack('трек-4');
+
+// playlist.updateRating(10);
+
+// console.log(playlist.getTrackCount());
+
+// console.log(playlist);
+
+// Задача 1
+
+// Створіть об'єкт "person" з властивостями "name", "age", "gender".
+//  Додайте до об'єкту метод, який буде виводити на екран повідомлення
+//  про особистість зі значеннями всіх властивостей.
+
+// const person ={
+//     name: 'Arsenii',
+//     age: 14,
+//     gender: 'male',
+//     getPersonData(){
+//         console.log(`Ваше ім'я ${this.name}, ваш вік ${this.age}, ваша стать ${this.gender}`);
+//     }
+// }
+// person.getPersonData();
