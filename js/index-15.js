56d053c5c8728b548b0e5e7cf4bc3ed11ebf6d9c
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2024-02-12 20:26:25 +0200

    .

---
+++ js/index-15.js
@@ -0,0 +1,59 @@
+// const feedback = {
+//     good: 5,
+//     neutral: 10,
+//     bad: 3,
+// };
+
+// const keys = Object.keys(feedback);
+// console.log(keys);
+
+// for (const key of keys){
+//     console.log(key);
+
+//     feedback[key];
+// }
+
+// const values = Object.values(feedback);
+// console.log(values);
+// for (const value of values){
+//     console.log(value);
+// }
+
+// console.log(feedback);
+
+// const friends = [
+//     {
+//     name: 'Mango',
+//     online: false
+//     },
+//     {
+//     name: 'Kiwi',
+//     online: true
+//     },
+//     {
+//     name: 'Poly', 
+//     online: false 
+//     },
+//     { 
+//     name: 'Ajax', 
+//     online: false 
+//     },
+// ];
+
+// for (const friend of friends){
+//     console.log(friend);
+// }    
+    
+// console.table(friends);
+
+// const findFriendByName = function (friends, name) {
+//     for (const friend of friends){
+//     console.log(friend.name);
+//     if (friend.name === 'Poly'){
+//         console.log('')
+//     }
+//     }   
+// }
+
+// console.log(findFriendByName(friends, 'Poly'));
+// console.log(findFriendByName(friends, 'Chelsy'));
