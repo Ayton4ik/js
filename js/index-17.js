9b6d39a86e0b929c0c0c26020b521bd8b47a3c32
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2024-03-01 20:28:22 +0200

    Update index-17.js

--- js/index-17.js
+++ js/index-17.js
@@ -32,3 +32,57 @@
 // })
 
 // console.log(total);
+
+// const players = [
+//     { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//     { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//     { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//     { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//     { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+// ];
+// console.table(players);
+
+// // const playerNames = players.map((player) => {
+// //     return player.name
+// // });
+// // console.log(playerNames);
+
+
+// const playerIdToUpdate = 'player-3';
+
+// const upatedPlayers = players.map((player) => {
+//     if (player.id === playerIdToUpdate){
+//         return {
+//             ...player,
+//             timePlayed: player.timePlayed + 100
+//         };
+//     }else {
+//         return {
+//             ...player
+//         };
+//     };
+// });
+// console.log(upatedPlayers);
+
+// Порахувати загальну кількість товарів в кошику
+
+// const cart = [
+// { label: 'Apples', price: 100, quantity: 2 },
+// { label: 'Bananas', price: 120, quantity: 3 },
+// { label: 'Lemons', price: 70, quantity: 4 },
+// ];
+
+// let totalAmount = 0;
+// cart.forEach((item) => {
+//     totalAmount += item.price * item.quantity;
+// });
+// console.log(totalAmount);
+
+// const newCart = cart.map((item) => {
+//     return {
+//         ...item,
+//         quantity: item.quantity + 1
+//     }
+// });
+// console.log(newCart);
+
