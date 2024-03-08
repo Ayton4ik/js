ed60da65ff51ea000edd0736a36738c852afc1ef
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2024-03-08 18:52:36 +0200

    Update index-17.js

--- js/index-17.js
+++ js/index-17.js
@@ -86,3 +86,28 @@
 // });
 // console.log(newCart);
 
+
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const filteredNumbers = numbers.filter((number) => {
+//     return number <= 10;
+// });
+// console.log(filteredNumbers);
+
+// const players = [
+//     { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//     { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//     { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//     { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//     { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
+// ];
+
+// const getOnlinePlayers = function (players) {
+//     const onlinePlayers = players.filter((player) => {
+//         return player.online;
+//     });
+//     return onlinePlayers;
+// };
+
+// console.log(getOnlinePlayers(players));
