d8ba77130a5392fccbab70e2b090be45dc93bbc8
Ayton4ik <129057995+Ayton4ik@users.noreply.github.com>
2024-04-05 20:23:34 +0300

    ,

---
+++ js/index-20.js
@@ -0,0 +1,10 @@
+// const targetBtn = document.querySelector('.js-target-btn');
+// const addListenerBtn = document.querySelector('.js-add-listener');
+// const removeListenerBtn = document.querySelector('.js-remove-listener');
+
+// addListenerBtn.addEventListener('click', onClick);
+// removeListenerBtn.removeEventListener('click', onClick);
+
+// function onClick() {
+//     console.log('Додати слухача');
+// }
